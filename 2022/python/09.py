166dce227a5d576544cc7d037d22905bd2374651
Michael Barrows <m.barrows@live.com>
2022-12-10 12:50:20 -0500

    day 9 part 2 solution

--- 2022/python/09.py
+++ 2022/python/09.py
@@ -10,8 +10,16 @@ R 4
 D 1
 L 5
 R 2"""
+long_test = """R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20"""
 
-tail_ajustments = {
+delta_adjustments = {
     '0,0':[0,0],
     '1,0':[0,0],
     '0,1':[0,0],
@@ -22,18 +30,21 @@ tail_ajustments = {
     '2,-1':[1,-1],
     '-2,-1':[-1,-1],
 
-
     '-1,2':[-1,1],
     '1,2':[1,1],
     '1,-2':[1,-1],
     '-1,-2':[-1,-1],
-
 
     '2,0':[1,0],
     '-2,0':[-1,0],
 
     '0,2':[0,1],
     '0,-2':[0,-1],
+
+    '2,2':[1,1],
+    '-2,-2':[-1,-1],
+    '2,-2':[1,-1],
+    '-2,2':[-1,1],
 }
 
 def move(knot,dir):
@@ -44,46 +55,51 @@ def move(knot,dir):
     return knot
     
 
-def tail_too_far(head:list,tail:list) -> None:
-    if abs(head[0] - tail[0]) == 2 and head[1] - tail[1] == 0:
+def knot_too_far(head:list,tail:list) -> None:
+    if abs(head[0] - tail[0]) == 2 and head[1] - tail[1] >= 0:
         return True
-    if abs(head[1] - tail[1]) == 2 and head[0] - tail[0] == 0:
+    if abs(head[1] - tail[1]) == 2 and head[0] - tail[0] >= 0:
         return True
-    if abs(head[1] - tail[1]) == 1 and abs(head[0] - tail[0]) == 2:
+    if abs(head[1] - tail[1]) >= 1 and abs(head[0] - tail[0]) == 2:
         return True
-    if abs(head[0] - tail[0]) == 1 and abs(head[1] - tail[1]) == 2:
+    if abs(head[0] - tail[0]) >= 1 and abs(head[1] - tail[1]) == 2:
         return True
     return False
 
-def adjust_tail(head:list,tail:list):
+def adjust_knot(head:list,tail:list):
     adjustment_key = f"{head[0]-tail[0]},{head[1]-tail[1]}"
-    tail = list(np.array(tail) + np.array(tail_ajustments.get(adjustment_key)))
+    tail = list(tail + np.array(delta_adjustments.get(adjustment_key)))
     return tail
-    
 
-
-def get_solution(input:Any) -> int:
-    head_pos = [0,0]
-    tail_pos = [0,0]
+def get_solution(input:Any,num_knots) -> int:
+    knot_pos = np.full((num_knots,2),0,int)
     tail_visited=[[0,0]]
     instructions = [x.split(' ') for x in input.split('\n')]
 
     for instruction in instructions:
         for _ in range(0,int(instruction[1])):
-            head_pos = move(head_pos,instruction[0])
-            if tail_too_far(head_pos,tail_pos):
-                tail_pos = adjust_tail(head_pos,tail_pos)
-            if tail_pos not in tail_visited:
-                tail_visited.append(tail_pos)
-    a,b = (len(tail_visited), None)
-    return a,b
+            knot_pos[0] = move(knot_pos[0],instruction[0])
+            for i in range(1,num_knots):
+                if knot_too_far(knot_pos[i-1],knot_pos[i]):
+                    knot_pos[i] = adjust_knot(knot_pos[i-1],knot_pos[i])
+            if list(knot_pos[num_knots-1]) not in tail_visited:
+                tail_visited.append(list(knot_pos[num_knots-1]))
+    a = len(tail_visited)
+    return a
 
 if __name__ == '__main__':
-    test_a,test_b = get_solution(test_input)
+    test_a = get_solution(test_input,2)
     print(test_a)
     assert test_a == 13
+    test_b = get_solution(test_input,10)
+    print(test_b)
+    assert test_b == 1
+    test_c = get_solution(long_test,10)
+    print(test_c)
+    assert test_c == 36
     d = get_data(day=9, year=2022)
-    a,b= get_solution(d)
+    a= get_solution(d,2)
+    b = get_solution(d,10)
     print(a,b)
     submit(a, part="a", day=9, year=2022)
-    # submit(b, part="b", day=6, year=2022)
+    submit(b, part="b", day=9, year=2022)
