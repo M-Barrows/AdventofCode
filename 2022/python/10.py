1fc5df7f38ee4708379e0791b2dd8bec0f28c1fe
Michael Barrows <m.barrows@live.com>
2022-12-12 12:06:32 -0500

    day 10 part b

--- 2022/python/10.py
+++ 2022/python/10.py
@@ -157,21 +157,70 @@ def get_cycle_actions(input:Any) -> int:
             current_cycle += 1
             register[current_cycle]=0 #noop
         else:
-            current_cycle += 2
+            current_cycle += 1
+            register[current_cycle]=0
+            current_cycle += 1
             register[current_cycle]=int(op.split(' ')[1])
     return register
+
+def get_sprite_position(register:dict) -> dict:
+    sprite_positions = {}
+    for current_key, current_val in register.items():
+        sprite_center = sum([v for (k,v) in register.items() if k <= current_key])
+        sprite_positions[current_key-1] = list(range(sprite_center-1,sprite_center+2))
+    return sprite_positions
+    
+def print_screen(sprite_positions:dict):
+    screen = [
+        [],
+        [],
+        [],
+        [],
+        [],
+        [],
+        []
+        ]
+    for cycle, position in sprite_positions.items():
+        screen[cycle//40].append('#' if cycle%40 in position else '.')
+    lines = [''.join(x) for x in screen[:6]]
+    picture = '\n'.join(lines)
+    print(picture)
 
 if __name__ == '__main__':
     zip_data = lambda cycles,register: [(c,register) for c in cycles]
     cycle_signal_strength = lambda data: sum([v for (k,v) in data[1].items() if k <= data[0]]) * data[0]
+    
     # Test Case
     test_output = get_cycle_actions(test_input.split('\n'))
     cycles = [20,60,100,140,180,220]
-    print(sum(list(map(cycle_signal_strength,zip_data(cycles,test_output)))))
+    assert 13140 == sum(list(map(cycle_signal_strength,zip_data(cycles,test_output))))
 
+    # Part A
     d = get_data(day=10, year=2022)
     register_a = get_cycle_actions(d.split('\n'))
     a = sum(list(map(cycle_signal_strength,zip_data(cycles,register_a))))
 
     submit(a, part="a", day=10, year=2022)
-    # submit(b, part="b", day=10, year=2022)
+    
+    # Part B
+    print_screen(get_sprite_position(test_output)) # Test Print to screen for input data
+    """
+    ##..##..##..##..##..##..##..##..##..##..
+    ###...###...###...###...###...###...###.
+    ####....####....####....####....####....
+    #####.....#####.....#####.....#####.....
+    ######......######......######......####
+    #######.......#######.......#######.....
+    """
+
+    sprite_positions = get_sprite_position(register_a)
+    print_screen(sprite_positions)
+    """
+    ###...##..#..#.####..##..#....#..#..##..
+    #..#.#..#.#..#.#....#..#.#....#..#.#..#.
+    #..#.#....####.###..#....#....#..#.#....
+    ###..#.##.#..#.#....#.##.#....#..#.#.##.
+    #....#..#.#..#.#....#..#.#....#..#.#..#.
+    #.....###.#..#.#.....###.####..##...###.
+    """
+
