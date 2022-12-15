33f9c16ebd29037a385de50aea3c6e9ebb18835f
Michael Barrows <m.barrows@live.com>
2022-12-15 16:05:09 -0500

    Part B solution
    Thanks to @hannahilea for the help on part B!

--- 2022/python/11.py
+++ 2022/python/11.py
@@ -3,8 +3,7 @@ from typing import Any
 import os
 import math
 from dataclasses import dataclass, field
-from pprint import PrettyPrinter
- 
+
 @dataclass
 class Monkey:
     name: str
@@ -20,26 +19,29 @@ class Monkey:
         self.items[0] = eval(self.operation_formula)
         self.total_inspects += 1
 
-    def reset_worry_level(self,val:bool):
-        if val:
+    def reset_worry_level_strategy(self,val:str,lcm:int):
+        if val=='a':
             self.items[0] = math.floor(self.items[0]/3)
+        if val=='b': #Credit to @hannahilea for their assistance on part B. https://github.com/hannahilea/AOC_2022/blob/main/Days/Day_11/src/Day_11.jl
+            self.items[0] = self.items[0]%lcm
     
     def test(self):
-        return self.items[0]%self.test_denominator == 0
+        test_remainder = self.items[0]%self.test_denominator
+        return test_remainder == 0
 
     def throw_to_monkey(self,target_monkey:str,troupe:list[dataclass]):
         _item = self.items.pop(0)
         m = [monkey for monkey in troupe if monkey.name == target_monkey][0]
         m.items.append(_item)
 
-    def process_items(self,troupe:list[dataclass],reset_worry_level:bool):
+    def process_items(self,troupe:list[dataclass],strategy:str,lcm:int):
         while len(self.items)>0:
             self.inspect()
-            self.reset_worry_level(reset_worry_level)
+            self.reset_worry_level_strategy(strategy,lcm)
             test_result = self.test()
             target = self.true_target if test_result else self.false_target
             self.throw_to_monkey(target,troupe)
-            self.process_items(troupe,reset_worry_level)
+            self.process_items(troupe,strategy,lcm)
     
 
 
@@ -71,38 +73,40 @@ def parse_input(input):
         del monkey_dict[monkey]['Test']
     [v.update({'name':k}) for k,v in monkey_dict.items()]
     monkey_list = [v for k,v in monkey_dict.items()]
-    return monkey_list
+    lcm = math.prod([monkey['test_denominator'] for monkey in monkey_list])
+    return monkey_list, lcm
 ### Operations for eval()
 
 ### Solution Synthesis
-def get_solution(troupe:list,rounds:int,reset_worry_level:bool) -> int:
+def get_solution(troupe:list,rounds:int,worry_reset_strategy:str,lcm:int) -> int:
     for i in range(0,rounds):
         for monkey in troupe:
-            monkey.process_items(troupe,reset_worry_level)
+            monkey.process_items(troupe,worry_reset_strategy,lcm)
     top_monkeys = [monkey.total_inspects for monkey in sorted(troupe,key = lambda monkey: -monkey.total_inspects)[:2]]
-    return top_monkeys[0]*top_monkeys[1]
+    return top_monkeys[0] * top_monkeys[1]
 
 if __name__ == '__main__':
-    monkey_list_a = parse_input(test_input)
+    monkey_list_a,lcm_a = parse_input(test_input)
     test_troupe_a = [Monkey(**monkey) for monkey in monkey_list_a]
-    test_result = get_solution(test_troupe_a,20,True)
+    test_result = get_solution(test_troupe_a,20,'a',lcm_a)
     assert 10605 == test_result
     print('Passed tests for part A!')
     
-    # monkey_list_b = parse_input(test_input)
-    # test_troupe_b = [Monkey(**monkey) for monkey in monkey_list_b]
-    # test_result_b = get_solution(test_troupe_b,10_000,False)
-    # assert 2713310158 == test_result_b
-    # print('Passed tests for part B!')
-
-    d = parse_input(get_data(day=11, year=2022))
-    troupe_a = [Monkey(**monkey) for monkey in d]
-    a= get_solution(troupe_a,20,True)
-    print(a)
-    submit(a, part="a", day=11, year=2022)
+    d = get_data(day=11, year=2022)
+    monkey_list_a,lcm_a = parse_input(d)
+    troupe_a = [Monkey(**monkey) for monkey in monkey_list_a]
+    result_a = get_solution(troupe_a,10_000,'a',lcm_a)
+    submit(result_a, part="a", day=11, year=2022)
     
-    # d = parse_input(get_data(day=11, year=2022))
-    # troupe_b = [Monkey(**monkey) for monkey in d]
-    # b= get_solution(troupe_b,10_000,False)
-    # print(b)
-    # submit(b, part="b", day=11, year=2022)
+    monkey_list_b,lcm_b = parse_input(test_input)
+    test_troupe_b = [Monkey(**monkey) for monkey in monkey_list_b]
+    test_result_b = get_solution(test_troupe_b,10_000,'b',lcm_b)
+    assert 2713310158 == test_result_b
+    print('Passed tests for part B!')
+
+    d = get_data(day=11, year=2022)
+    monkey_list_b,lcm_b = parse_input(d)
+    troupe_b = [Monkey(**monkey) for monkey in monkey_list_b]
+    result_b = get_solution(troupe_b,10_000,'b',lcm_b)
+    submit(result_b, part="b", day=11, year=2022)
+    
